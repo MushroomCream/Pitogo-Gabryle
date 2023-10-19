114d93f52edddaf5747d602eb00c1d3501ee93de
Gabryle Antonie Pitogo <148405650+MushroomCream@users.noreply.github.com>
2023-10-19 19:13:45 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,15 @@
+public class FavoriteSong {
+    public static void main(String[]args) {
+        //Print Song Lyrics
+        PrintSong ps = new PrintSong();
+        ps.Lyric();
+    }
+}
+class PrintSong {
+    public void Lyric() {
+        System.out.println("What I've felt, what I've known");
+        System.out.println("Never shined through in what I've shown");
+        System.out.println("Never be, never see");
+        System.out.println("Won't see what might have been");
+    }
+}
---
+++ PRELIM/TableAndChairs.java
@@ -0,0 +1,17 @@
+public class TableAndChairs {
+    public static void main(String[]args) {
+        //Print Chairs and Table
+        Printf pf = new Printf();
+        pf.TNC();
+    }
+}
+class Printf {
+    public void TNC() {
+        System.out.println("X                    X");
+        System.out.println("X                    X");
+        System.out.println("X     XXXXXXXXXX     X");
+        System.out.println("XXXXX X        X XXXXX");
+        System.out.println("X   X X        X X   X");
+        System.out.println("X   X X        X X   X");
+    }
+}
